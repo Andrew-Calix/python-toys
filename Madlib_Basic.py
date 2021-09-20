fcd07f9f90e591707524d40b38f15a7ceef61f96
Andrew-Calix <83693460+Andrew-Calix@users.noreply.github.com>
2021-09-20 15:16:22 +0530

    Add files via upload

---
+++ Madlib_Basic.py
@@ -0,0 +1,15 @@
+# Take a paragraph from online and remove the adjectives,verbs and nouns
+# Create variables for the empty places
+# Create an input variable to prompt the user for the word
+# Finally execute it
+
+var1 = input("Please enter a random adjective: ")
+var2 = input("Please enter a delicious word: ")
+var3 = input("Enter a scary word: ")
+
+print(
+    f"     whenever you would like to {var1} your created portfolio repository\n"
+    f"     make them locally and push the {var2} to your remote repository using the commands in step \n"
+    f"     do not {var3} the changes directly to the pages")
+
+input('Press ENTER to exit')
